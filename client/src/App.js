2525d4e93e5714ef54ba63d4a0898d1b8f628a6e
LAPTOP-5Q44CSU7\reika <reikal951@gmail.com>
2023-03-09 13:13:50 +0200

    FrontEnd: Created log in page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import { Home, Register } from "./pages"
+import { Home, Register, Login } from "./pages"
 import { GlobalStyle } from "./GlobalStyles.styles";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Register/>
+      <Login/>
     </>
   );
 }
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,29 @@
+import { useState } from "react"
+import { MainStyled } from "./styles/Register.styles"
 
 function Login() {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        alert(`
+        Usr: ${username}\n
+        Psw: ${password}\n`)
+    }
+
   return (
-    <div>Login</div>
+        <MainStyled>
+            <div>Login</div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="username">Enter username: </label>
+                <input type="text" id="username" value={username} onChange={ (e) => setUsername( e.target.value)}/> 
+
+                <label htmlFor="password">Enter password: </label>
+                <input type="password" id="password" value={password} onChange={ (e) => setPassword( e.target.value)}/>
+
+                <button>Log In</button>
+            </form>
+        </MainStyled>
   )
 }
 
