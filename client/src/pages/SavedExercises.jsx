691149de1bb6a9e6cd37d8c6ae9614f78adb6c84
LAPTOP-5Q44CSU7\reika <reikal951@gmail.com>
2023-05-06 18:54:28 +0300

    FronEnd: Added search inside Saved Exercises

--- client/src/pages/SavedExercises.jsx
+++ client/src/pages/SavedExercises.jsx
@@ -7,6 +7,12 @@ import { getExercieses, postExerciese, publishExerciese, deleteExerciese, remove
 
 function SavedExercises() {
   const[popUp,setPopUp] = useState(false)
+
+  const [search,setSearch] = useState({
+    field: "",
+    bodyPart: ""
+  });
+
   const dispatch = useDispatch()
 
   const {savedExercises, isLoading, isSuccess, isError, message } = useSelector(
@@ -42,6 +48,13 @@ function SavedExercises() {
 
   const getSearchData = (data) => {
     console.log(data)
+    setSearch(prevState =>({
+      ...prevState,
+      field: data.field,
+      bodyPart: data.bodyPart !== "all" ? data.bodyPart : ""
+    }))
+    console.log(search)
+    
   }
 
   const getCreateData = (data) => {
@@ -59,7 +72,7 @@ function SavedExercises() {
   if(isLoading){
     return <Loading size={"100"} speed={"4"} />
  }
-
+  console.log(savedExercises.length)
   return (
     <SavedExercisesStyled>
       <h2>Saved Exercises</h2> 
@@ -69,8 +82,13 @@ function SavedExercises() {
       {
         savedExercises.length === 0 && <h4>No exercises</h4>
       }
-      {
-      savedExercises.map(object => {
+      {savedExercises.filter(object => (
+        object.name.toLowerCase().includes(search.field)
+        && object.bodyParts.join("").toLowerCase().includes(search.bodyPart)
+        
+      )
+      )
+      .map(object => {
         return( 
           <Exercise key={object._id} {...object} public={false} onDelete={handleDelete} onRemove={handleRemove} onPublish={handlePublish}/>
           )
