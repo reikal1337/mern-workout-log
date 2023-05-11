c760fa0e8a46bcc3d3890e57a1b67cfe5c4c1514
LAPTOP-5Q44CSU7\reika <reikal951@gmail.com>
2023-05-11 19:39:35 +0300

    FrontEnd: Added better validations. Allows letters number spaces and "-". BackEnd: Added post/create workout.

--- client/src/pages/Workouts.jsx
+++ client/src/pages/Workouts.jsx
@@ -1,31 +1,68 @@
 import { WorkoutsStyled } from "./styles/Workouts.style"
-import { Workout, WorkoutSearch, CreateWorkouteForm } from "../components"
+import { Workout, WorkoutSearch, CreateWorkouteForm, Loading } from "../components"
 import mockData from "../components/mockWorkoutData";
 import { SimpleButtonBlue } from "../components/styles/Buttons.syles";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getWorkouts, reset } from "../features/workouts/workoutsSlice";
 
 
 function Workouts() {
   const[popUp,setPopUp] = useState(false)
+
+  const dispatch = useDispatch();
+
+  const { workouts, isLoading, isSuccess, isError, message } = useSelector(
+    (state) => state.workouts
+  )
+
+  useEffect(() => {
+    dispatch(getWorkouts())
+    if(isError){
+      console.log(message)//Change to diplay error...
+    }
+
+    return () => {
+      dispatch(reset())
+    }
+  },[dispatch])
 
   const getSearchData = (data) => {
     console.log(data)
   }
 
   const getCreateData = (data) => {
-    console.log(data)
+    
+    setPopUp(false)
   }
 
   const handleCreateButton = () => {
     setPopUp(true)
   }
 
+  if(isLoading){
+    return <Loading size={"100"} speed={"4"} />
+ }
+ console.log(workouts.length)
+ console.log(workouts)
+ 
   return (
     <WorkoutsStyled>
         <h2>Workouts</h2>
         <WorkoutSearch onSubmit={getSearchData}/>
         <SimpleButtonBlue onClick={handleCreateButton} id="button-create">Create Workout</SimpleButtonBlue>
         <CreateWorkouteForm popUp={popUp} setPopUp={setPopUp} onSubmit={getCreateData}></CreateWorkouteForm>
+        {
+          workouts.length === 0 && <h4>No Workouts</h4>
+        }
+        {
+          workouts.map(object => {
+            return( 
+              <Workout key={object._id} {...object} />
+              )
+            
+          })
+        }
         {mockData.map( object => {
           return(
             <Workout key={object.id} {...object} />
