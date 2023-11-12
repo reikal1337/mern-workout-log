d5c756858f2d19abcbecd822d1eba3d2d33050cb
LAPTOP-5Q44CSU7\reika <reikal951@gmail.com>
2023-11-12 22:29:03 +0200

    Front: updating link again...Thats why you test  before deployment..

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,10 @@
 import { FooterStyled } from "./styles/Footer.style"
-import { Link } from "react-router-dom"
 
 function Footer() {
   return (
     <FooterStyled>
         <p>Copyright &copy; 2022-{(new Date().getFullYear())} All Rights Reserved by
-        <Link to={{ pathname: "https://lukasreika.com/" }} target="_blank"> Reika Developments</Link></p>
+        <a href="https://lukasreika.com/" target="_blank" rel="noopener noreferrer"> Reika Developments</a></p>
     </FooterStyled>
   )
 }
